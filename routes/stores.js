d9d40e8781b7ebe5a875814ff87450b49cd1aece
dmytrotrydub <tridub.d87@gmail.com>
2022-09-11 23:45:36 -0400

    Created template for discount email prompt

--- routes/stores.js
+++ routes/stores.js
@@ -5,5 +5,6 @@ const nodeMailer = require('../controllers/nodeMailer');
 router.get('/', storeController.getStores);
 router.post('/email', nodeMailer.getEmailBody);
 router.post('/item/:id', nodeMailer.getPurchaseInfo);
+router.post('/discount', nodeMailer.getDiscount);
 
 module.exports = router;
